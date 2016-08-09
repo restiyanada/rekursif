7f292e876070ace72f932828b20c31d0562265a5
Restiyana Dwi Astuti <restiyana.dwi@gmail.com>
2016-08-09 19:07:48 +0700

    Versi 1.4

--- faktorial.c
+++ faktorial.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-int faktorial(int a){
-	if(a==0 || a==1) return 1;
-	return a*faktorial(a-1);
+int faktorial(int x){
+	if(x==0 || x==1) return 1;
+	return x*faktorial(x-1);
 }
 main(){
 	printf("%d", faktorial(6));
