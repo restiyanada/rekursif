01f12faeb5716435813a246cb883ff54e1336212
Restiyana Dwi Astuti <restiyana.dwi@gmail.com>
2016-08-09 18:26:55 +0700

    Versi 5.1
    Update

--- cek bilangan prima.c
+++ cek bilangan prima.c
@@ -3,20 +3,20 @@
 
 int n;
 
-int cekPrima(int p){
+int cekbilanganPrima(int p){
    if(p == 1){
       return 1;
    }else if(n%p==0){
       return 0;
    }else{
-      return cekPrima(p-1);
+      return cekbilanganPrima(p-1);
    }
 }
 
 main(){
    printf("masukkan nilai :"); scanf("%d",&n);
    if(n>1){
-      int p = cekPrima(n-1);
+      int p = cekbilanganPrima(n-1);
       if(p == 1){
          printf("%d bilangan PRIMA\n",n);
       }else{
