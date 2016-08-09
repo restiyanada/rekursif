2c7f2d7dae9e5bec70e083fd841b128b60c9f8f4
Restiyana Dwi Astuti <restiyana.dwi@gmail.com>
2016-08-09 18:24:54 +0700

    Versi 6

---
+++ deretf.c
@@ -0,0 +1,28 @@
+#include 
+#include 
+
+int n;
+
+int cekPrima(int p){
+   if(p == 1){
+      return 1;
+   }else if(n%p==0){
+      return 0;
+   }else{
+      return cekPrima(p-1);
+   }
+}
+
+main(){
+   printf("masukkan nilai :"); scanf("%d",&n);
+   if(n>1){
+      int p = cekPrima(n-1);
+      if(p == 1){
+         printf("%d bilangan PRIMA\n",n);
+      }else{
+         printf("%d bilangan BUKAN PRIMA\n",n);
+      }
+   }else{
+      printf("%d bilangan BUKAN PRIMA\n",n);
+   }
+}
