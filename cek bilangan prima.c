3f5cc9755108f665519f1c59e0b503cb559a8b98
Restiyana Dwi Astuti <restiyana.dwi@gmail.com>
2016-08-09 18:37:15 +0700

    versi 5.2
    typos fixed

--- cek bilangan prima.c
+++ cek bilangan prima.c
@@ -3,21 +3,21 @@
 
 int n;
 
-int cekbilanganPrima(int p){
-   if(p == 1){
+int cekbilanganPrima(int a){
+   if(a == 1){
       return 1;
-   }else if(n%p==0){
+   }else if(n%a==0){
       return 0;
    }else{
-      return cekbilanganPrima(p-1);
+      return cekbilanganPrima(a-1);
    }
 }
 
 main(){
    printf("masukkan nilai :"); scanf("%d",&n);
    if(n>1){
-      int p = cekbilanganPrima(n-1);
-      if(p == 1){
+      int a = cekbilanganPrima(n-1);
+      if(a == 1){
          printf("%d bilangan PRIMA\n",n);
       }else{
          printf("%d bilangan BUKAN PRIMA\n",n);
